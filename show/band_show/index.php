24ff931c15c7a84c37598d0c35cbe4a02b26f974
Yasuhiro0127 <takeuchiys@scft.co.jp>
2025-06-18 14:46:58 +0900

    show.phpに追加

--- show/band_show/index.php
+++ show/band_show/index.php
@@ -13,7 +13,11 @@ try {
     event_dates.event_date AS event_date,
     band_event_entries.id AS  event_entries_id,
     event_dates.id AS event_dates_id
-    
+    band_event_entries.representative_name AS representative_name,
+    band_event_entries.line_id AS line_id,
+    band_event_entries.performance_time AS performance_time
+
+
     FROM bands
     LEFT JOIN band_event_entries ON bands.id = band_event_entries.band_id
     LEFT JOIN event_dates ON band_event_entries.event_id = event_dates.id;";
@@ -23,13 +27,16 @@ try {
 
     if (isset($_GET['sort'])) {
         if ($_GET['sort'] == "ライブ日昇順") {
-            $sql = "SELECT
+            $sql = "SELECT 
             bands.time AS time,
             bands.name AS name,
             bands.organization AS organization,
             event_dates.event_date AS event_date,
             band_event_entries.id AS  event_entries_id,
             event_dates.id AS event_dates_id
+            band_event_entries.representative_name AS representative_name,
+            band_event_entries.line_id AS line_id,
+            band_event_entries.performance_time AS performance_time
             FROM bands
             LEFT JOIN band_event_entries ON bands.id = band_event_entries.band_id
             LEFT JOIN event_dates ON band_event_entries.event_id = event_dates.id ORDER BY event_date DESC;";
@@ -42,6 +49,9 @@ try {
             event_dates.event_date AS event_date,
             band_event_entries.id AS  event_entries_id,
             event_dates.id AS event_dates_id
+            band_event_entries.representative_name AS representative_name,
+            band_event_entries.line_id AS line_id,
+            band_event_entries.performance_time AS performance_time
             FROM bands
             LEFT JOIN band_event_entries ON bands.id = band_event_entries.band_id
             LEFT JOIN event_dates ON band_event_entries.event_id = event_dates.id ;";
@@ -57,6 +67,9 @@ try {
         event_dates.event_date AS event_date,
         band_event_entries.id AS  event_entries_id,
         event_dates.id AS event_dates_id
+        band_event_entries.representative_name AS representative_name,
+        band_event_entries.line_id AS line_id,
+        band_event_entries.performance_time AS performance_time
         FROM bands
         LEFT JOIN band_event_entries ON bands.id = band_event_entries.band_id
         LEFT JOIN event_dates ON band_event_entries.event_id = event_dates.id WHERE name = :name;";
@@ -113,6 +126,10 @@ try {
                 <th>ライブ日</th>
                 <th>バンド名</th>
                 <th>所属団体</th>
+                <th>申し込み</th>
+                <th>LINE ID</th>
+                <th>パフォーマンスタイム</th>
+
             </tr>
             <?php foreach ($bands as $row): ?>
                 <tr>
@@ -122,6 +139,9 @@ try {
                     </td>
                     <td><?php echo htmlspecialchars($row['name']); ?></td>
                     <td><?php echo htmlspecialchars($row['organization']); ?></td>
+                    <td><?php echo htmlspecialchars($row['representative_name']); ?></td>
+                    <td><?php echo htmlspecialchars($row['line_id']); ?></td>
+                    <td><?php echo htmlspecialchars($row['performance_time']); ?></td>
                 </tr>
             <?php endforeach; ?>
         </table>
